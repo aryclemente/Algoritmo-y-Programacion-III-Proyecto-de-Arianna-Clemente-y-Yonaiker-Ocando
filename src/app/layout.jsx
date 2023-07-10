9b8dd033674e4f09fc6bf519a26c443507b3a59f
Yonaiker Ocando <yonaiker2410@gmail.com>
2023-07-09 23:04:36 -0400

    add config daisyui

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./src/router/AppRouter";
+import "../app/globals.css";
 
 ReactDOM.render(<AppRouter />, document.getElementById("root"));
