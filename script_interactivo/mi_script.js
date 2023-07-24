bf34c8f73d2843053fc4e1b379cf675dbc60751c
Yonaiker Ocando <yonaiker2410@gmail.com>
2023-07-24 01:07:29 -0400

    correccion de errores explicitos

--- script_interactivo/mi_script.js
+++ script_interactivo/mi_script.js
@@ -1,22 +1,31 @@
-import fs from 'fs';
-import inquirer from 'inquirer';
-   
-   inquirer.prompt([
+import fs from "fs";
+import inquirer from "inquirer";
+import path from "path";
+
+inquirer
+  .prompt([
     {
-        type: 'input',
-        name: 'filepath',
-        message: 'Por favor, introduce la ruta del archivo que deseas manipular:'
-    }
-])
-.then(answers => {
-    fs.readFile(answers.filepath, 'utf8', (err, data) => {
-        if (err) throw err;
+      type: "input",
+      name: "filepath",
+      message:
+        "Por favor, introduce la ruta especifica del archivo que deseas manipular:",
+    },
+  ])
+  .then((answers) => {
+    fs.readFile(answers.filepath, "utf8", (err, data) => {
+      if (err) {
+        console.error("Error al leer el archivo:", err);
+        return;
+      }
 
-        const newData = data.toUpperCase();
+      const newData = data.toUpperCase();
 
-        fs.writeFile('new_file.txt', newData, err => {
-            if (err) throw err;
-            console.log('¡Archivo guardado con éxito!');
-        });
+      fs.writeFile("new_file.txt", newData, (err) => {
+        if (err) {
+          console.error("Error al guardar el resultado:", err);
+          return;
+        }
+        console.log("¡Archivo guardado con éxito!");
+      });
     });
-});
+  });
