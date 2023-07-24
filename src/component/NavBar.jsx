ad2be93f4d45c1dbf8530aaabc00005af084fa45
aryclemente <hello.aryc@gmail.com>
2023-07-24 03:27:47 -0400

    Cambios en el NavBar

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -5,13 +5,21 @@ const NavBar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">System-Fact</a>
+        <a className="btn btn-ghost text-xl">IUJO Systems</a>
       </div>
+
+      <div className="flex-1 px-2 text-center ">
+        <a className="btn btn-ghost ">Inicio</a>
+        <a className="btn btn-ghost">Facturación</a>
+        <a className="btn btn-ghost">Productos</a>
+        <a className="btn btn-ghost">Contacto</a>
+      </div>
+
       <div className="flex-none">
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
-              <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              <img src="https://www.flaticon.es/icono-gratis/usuario_1177568?term=usuario&page=1&position=2&origin=tag&related_id=1177568" />
             </div>
           </label>
           <ul
@@ -20,15 +28,15 @@ const NavBar = () => {
           >
             <li>
               <a className="justify-center">
-                Profile
-                <span className="badge">New</span>
+                Perfil
+                
               </a>
             </li>
             <li>
-              <a>Settings</a>
+            <a className="justify-center">Configuración</a>
             </li>
             <li>
-              <a>Logout</a>
+            <a className="justify-center">Cerrar Sesión</a>
             </li>
           </ul>
         </div>
