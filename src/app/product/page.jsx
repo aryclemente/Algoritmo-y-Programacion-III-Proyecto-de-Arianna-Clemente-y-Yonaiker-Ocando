0f405d3b3ae263b74d9c485d0f2f35ff0aa9e430
Yonaiker Ocando <yonaiker2410@gmail.com>
2023-08-14 00:16:16 -0400

    .

---
+++ src/app/product/page.jsx
@@ -0,0 +1,14 @@
+import Aside from "@/component/Aside";
+import List from "@/component/Product/List";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Aside />
+      <List />
+    </>
+  );
+};
+
+export default page;
