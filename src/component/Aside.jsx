9e030896ce1465cf640b5d107f7035d0292e886a
yonaiker <yonaiker.ocando@unidigital.global>
2023-08-15 09:06:50 -0400

    .

--- src/component/Aside.jsx
+++ src/component/Aside.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Link from "next/link";
+import { CgMenuGridO } from "react-icons/cg";
+import { FaRegFolderOpen } from "react-icons/fa";
+
 const Aside = () => {
   return (
     <aside className="fixed top-0 z-10 ml-[-100%] flex h-screen w-full flex-col justify-between border-r bg-white px-6 pb-3 transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%] dark:bg-gray-800 dark:border-gray-700">
@@ -24,81 +27,38 @@ const Aside = () => {
 
         <ul className="mt-8 space-y-2 tracking-wide">
           <li>
-            <a
-              href="#"
+            <Link
+              href={"/"}
               aria-label="dashboard"
               className="relative flex items-center space-x-4 rounded-xl bg-gradient-to-r from-sky-600 to-cyan-400 px-4 py-3 text-white"
             >
-              <svg className="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
-                <path
-                  d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z"
-                  className="dark:fill-slate-600 fill-current text-cyan-400"
-                ></path>
-                <path
-                  d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z"
-                  className="fill-current text-cyan-200 group-hover:text-cyan-300"
-                ></path>
-                <path
-                  d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z"
-                  className="fill-current group-hover:text-sky-300"
-                ></path>
-              </svg>
+              <div className="text-xl">
+                <CgMenuGridO />
+              </div>
               <span className="-mr-1 font-medium">Inicio</span>
-            </a>
+            </Link>
           </li>
           <li>
             <Link
               href={"/product"}
               className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600 dark:text-gray-300"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  className="fill-current text-gray-300 group-hover:text-cyan-300"
-                  fillRule="evenodd"
-                  d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z"
-                  clipRule="evenodd"
-                />
-                <path
-                  className="fill-current text-gray-600 group-hover:text-cyan-600 dark:group-hover:text-sky-400"
-                  d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z"
-                />
-              </svg>
+              <CgMenuGridO className="text-xl" />
               <span className="group-hover:text-gray-700 dark:group-hover:text-gray-50">
                 Productos
               </span>
             </Link>
           </li>
           <li>
-            <a
+            <Link
               href="#"
               className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600 dark:text-gray-300"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  className="fill-current text-gray-300 group-hover:text-cyan-300"
-                  fillRule="evenodd"
-                  d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z"
-                  clipRule="evenodd"
-                />
-                <path
-                  className="fill-current text-gray-600 group-hover:text-cyan-600 dark:group-hover:text-sky-400"
-                  d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z"
-                />
-              </svg>
+              <FaRegFolderOpen className="text-xl" />
               <span className="group-hover:text-gray-700 dark:group-hover:text-gray-50">
                 Documentos
               </span>
-            </a>
+            </Link>
           </li>
           <li>
             <a
