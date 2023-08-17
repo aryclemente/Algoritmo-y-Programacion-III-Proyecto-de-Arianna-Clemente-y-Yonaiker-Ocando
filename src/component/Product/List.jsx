fd55622fc59534ac18c9f73b879a664822d92470
yonaiker <yonaiker.ocando@unidigital.global>
2023-08-17 17:02:27 -0400

    .

--- src/component/Product/List.jsx
+++ src/component/Product/List.jsx
@@ -5,21 +5,33 @@ import product from "../../data/product.json";
 const List = () => {
   const [currencyProduct, setcurrencyProduct] = useState(product);
 
-  const showDetail = () => {};
-
   // console.log(currencyProduct);
   return (
     <div className="p-8">
+      {/* // */}
+      <input type="checkbox" id="my-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">
+            Congratulations random Internet user!
+          </h3>
+          <p className="py-4">
+            You've been selected for a chance to get one year of subscription to
+            use Wikipedia for free!
+          </p>
+          <div className="modal-action">
+            <label htmlFor="my-modal" className="btn">
+              Yay!
+            </label>
+          </div>
+        </div>
+      </div>
+      {/* // */}
       <div className="overflow-x-auto">
         <table className="table">
           {/* head */}
           <thead>
             <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
               <th>Nombre</th>
               <th>Precio</th>
               <th>Categoría</th>
@@ -30,11 +42,6 @@ const List = () => {
             {/* row 1 */}
             {currencyProduct.map((product) => (
               <tr>
-                <th>
-                  <label>
-                    <input type="checkbox" className="checkbox" />
-                  </label>
-                </th>
                 <td>
                   <div className="flex items-center space-x-3">
                     <div className="avatar">
@@ -53,9 +60,9 @@ const List = () => {
                 <td>{product.price}</td>
                 <td>{product.category}</td>
                 <th>
-                  <button className="btn btn-ghost btn-xs" onClick={showDetail}>
+                  <label className="btn btn-ghost btn-xs" htmlFor="my-modal">
                     detalles
-                  </button>
+                  </label>
                 </th>
               </tr>
             ))}
@@ -63,7 +70,6 @@ const List = () => {
           {/* foot */}
           <tfoot>
             <tr>
-              <th></th>
               <th>Nombre</th>
               <th>Precio</th>
               <th>Categoría</th>
