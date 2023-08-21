e99b3a59ba0affef6aaa98693cfe7c9f7ed3f24b
Yonaiker Ocando <yonaiker2410@gmail.com>
2023-08-20 23:34:35 -0400

    add paginacion a la lista de productos

--- src/component/Product/List.jsx
+++ src/component/Product/List.jsx
@@ -1,13 +1,32 @@
 "use client";
 import React, { useState } from "react";
 import product from "../../data/product.json";
+import { AiFillPlusCircle } from "react-icons/ai";
 /* use client */
 const List = () => {
-  const [currencyProduct, setcurrencyProduct] = useState(product);
+  const [currencyProduct, setCurrencyProduct] = useState(product);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const productsPerPage = 15;
+
+  const totalPages = Math.ceil(currencyProduct.length / productsPerPage);
+
+  // Calcula el índice inicial y final de los productos a mostrar en la página actual
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = currencyProduct.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
 
   // console.log(currencyProduct);
   return (
-    <div className="p-8">
+    <div className="p-8 ">
+      <div className=" text-end pe-5">
+        <button className="btn text-xl">
+          <AiFillPlusCircle />
+        </button>
+      </div>
       {/* // */}
       <input type="checkbox" id="my-modal" className="modal-toggle" />
       <div className="modal">
@@ -32,7 +51,8 @@ const List = () => {
           {/* head */}
           <thead>
             <tr>
-              <th>Nombre</th>
+              <th className="">ID</th>
+              <th className="text-center">Nombre</th>
               <th>Precio</th>
               <th>Categoría</th>
               <th></th>
@@ -40,16 +60,16 @@ const List = () => {
           </thead>
           <tbody>
             {/* row 1 */}
-            {currencyProduct.map((product) => (
-              <tr>
+            {currentProducts.map((product) => (
+              <tr key={product.id}>
                 <td>
-                  <div className="flex items-center space-x-3">
+                  <div className="">{product.id}</div>
+                </td>
+                <td>
+                  <div className="flex items-center space-x-14">
                     <div className="avatar">
                       <div className="mask mask-squircle w-12 h-12">
-                        <img
-                          src={product.imagen}
-                          alt="Avatar Tailwind CSS Component"
-                        />
+                        <img src={product.imagen} alt="photo product" />
                       </div>
                     </div>
                     <div>
@@ -77,6 +97,23 @@ const List = () => {
             </tr>
           </tfoot>
         </table>
+      </div>
+
+      {/* listado */}
+      <div className="text-center">
+        <div className="join">
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index + 1}
+              className={`join-item btn ${
+                currentPage === index + 1 ? "btn-active" : ""
+              }`}
+              onClick={() => setCurrentPage(index + 1)}
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
