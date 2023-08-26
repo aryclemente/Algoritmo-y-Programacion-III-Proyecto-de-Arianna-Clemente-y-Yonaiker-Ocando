b348766d758c5269027a535b3aacbf8775c08d85
aryclemente <hello.aryc@gmail.com>
2023-08-26 18:55:55 -0400

    Modal clientes

--- src/app/clients/page.jsx
+++ src/app/clients/page.jsx
@@ -1,11 +1,8 @@
-/* use client */
+"use client";
 
-/* import Newclient from "@/component/Clients/Newclient";
-import Statsclient from "@/component/Clients/Statsclient";
- */
+import React from "react";
 import Button from "@/component/Clients/ButtonClient";
 import Listclients from "@/component/Clients/List";
-import React from "react";
 
 const Client = () => {
   return (
