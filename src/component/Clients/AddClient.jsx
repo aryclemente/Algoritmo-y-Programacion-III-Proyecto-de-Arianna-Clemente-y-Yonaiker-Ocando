7650c261e639f26f152040efd14842459ad8bff6
aryclemente <hello.aryc@gmail.com>
2023-08-28 10:12:06 -0400

    Nuevo cliente v3

--- src/component/Clients/AddClient.jsx
+++ src/component/Clients/AddClient.jsx
@@ -47,14 +47,13 @@ const ButtonWithModal = () => {
           className=" drop-shadow-md bg-gradient-to-r from-sky-600 to-cyan-400 text-white font-bold px-5 py-4 rounded-full"
           onClick={openModal}
         >
-          <FaUserPlus className="mr-2" />
-          Nuevo Cliente
+          <FaUserPlus className="mr-2" /> Nuevo Cliente
         </button>
       </div>
       {isModalOpen && (
         // el otro
         <div className="fixed inset-0 flex items-center justify-center z-50 m-auto">
-          <div className="bg-slate-800 rounded-lg drop-shadow-xl p-8">
+          <div className="bg-white rounded-lg drop-shadow-xl p-8">
             <button className="btn btn-circle float-right" onClick={closeModal}>
               <GrClose />
             </button>
@@ -125,6 +124,40 @@ const ButtonWithModal = () => {
                     class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
                     Correo Electrónico
+                  </label>
+                </div>
+
+                <div class="relative z-0 w-full mb-10 group">
+                  <input
+                    type="date"
+                    name="date"
+                    id="date"
+                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                    placeholder=" "
+                    required
+                  />
+                  <label
+                    for="date"
+                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                  >
+                    Fecha de Nacimiento
+                  </label>
+                </div>
+
+                <div class="relative z-0 w-full mb-10 group">
+                  <input
+                    type="textbox"
+                    name="adrees"
+                    id="adress"
+                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                    placeholder=" "
+                    required
+                  />
+                  <label
+                    for="adress"
+                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                  >
+                    Dirección
                   </label>
                 </div>
               </div>
