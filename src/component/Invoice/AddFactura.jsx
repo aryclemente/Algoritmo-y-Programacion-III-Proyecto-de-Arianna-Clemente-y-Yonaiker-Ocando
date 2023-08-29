c531458079824ec7976f2e070634c4a244c79201
yonaiker <yonaiker.ocando@unidigital.global>
2023-08-29 17:29:35 -0400

    buscar clientes por cedula

--- src/component/Invoice/AddFactura.jsx
+++ src/component/Invoice/AddFactura.jsx
@@ -9,12 +9,18 @@ const AddFactura = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [selectedClient, setSelectedClient] = useState(null);
 
-  const handleClientSearch = () => {
+  const handleClientSearch = (event) => {
     const cedula = event.target.value;
 
-    const foundClient = clients.find((client) => client.cedula === cedula);
+    const foundClient = clients.find((client) =>
+      client.ci.toString().startsWith(cedula)
+    );
 
-    setSelectedClient(foundClient);
+    if (foundClient) {
+      setSelectedClient(foundClient);
+    } else {
+      alert("nada");
+    }
   };
   const click = () => {
     console.log("hola mundo", selectedClient);
@@ -26,26 +32,33 @@ const AddFactura = () => {
         {/* columna izqueirda */}
         <div className="">
           <form>
-            <div class="grid md:grid-cols-3 md:gap-6">
-              <div class="relative z-0 w-full mb-6 group">
-                <div className="display">
+            <div className="grid md:grid-cols-3 md:gap-6">
+              <div className="relative z-0 w-full group">
+                <div className="flex">
                   <input
+                    list="listClient"
+                    name="browser"
+                    id="browser"
                     type="text"
-                    name="cedula"
-                    id="cedula"
                     class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     placeholder=" "
                     required
-                    onBlur={handleClientSearch}
+                    autoComplete="true"
+                    onInput={handleClientSearch}
                   />
+                  <datalist className="" id="listClient">
+                    {clients.map((client) => (
+                      <option value={client.ci} />
+                    ))}
+                  </datalist>
                   <label
                     for="floating_first_name"
-                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
-                    Cliente
+                    Cédula de Identidad
                   </label>
 
-                  <div className="text-2xl p-2">
+                  <div className=" text-2xl p-2">
                     <Link href={"http://localhost:3001/clients"}>
                       <AiFillPlusCircle />
                     </Link>
@@ -56,8 +69,9 @@ const AddFactura = () => {
               {selectedClient && (
                 <div className="mt-4">
                   <h3>Cliente Encontrado:</h3>
-                  <p>Nombre: {selectedClient.name}</p>
-                  <p>Cédula: {selectedClient.cedula}</p>
+                  <p>Nombre: {selectedClient.first_name}</p>
+                  <p>Nombre: {selectedClient.last_name}</p>
+                  <p>Cédula: {selectedClient.ci}</p>
                   {/* Resto de los detalles del cliente */}
                 </div>
               )}
