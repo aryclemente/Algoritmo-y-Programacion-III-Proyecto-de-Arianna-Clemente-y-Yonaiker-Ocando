1bde7d691f30cbf3a88e2e5cddbee2e8220fe070
aryclemente <hello.aryc@gmail.com>
2023-09-01 13:47:35 -0400

    save

--- src/component/Clients/Stats.jsx
+++ src/component/Clients/Stats.jsx
@@ -8,10 +8,7 @@ const CustomerStatistics = () => {
     <> 
         <div className="px-6 pt-6 2xl:container">
             <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-
-
-
-            <div className="md:col-span-2 lg:col-span-1">
+                <div className="md:col-span-2 lg:col-span-1">
                     <div className="h-full space-y-6 group p-6 sm:p-8 rounded-3xl bg-white border border-gray-200/50 dark:shadow-none dark:border-gray-700 dark:bg-gray-800 bg-opacity-50 shadow-2xl shadow-gray-600/10">
                         <h5 className="text-xl text-gray-700 dark:text-gray-300">
                         Total de Clientes
@@ -23,10 +20,7 @@ const CustomerStatistics = () => {
                         </div>    
                     </div> 
                 </div> 
-                
-                        
-                    
-                
+                               
                 <div className="md:col-span-2 lg:col-span-1">
                     <div className="h-full space-y-6 group p-6 sm:p-8 rounded-3xl bg-white border border-gray-200/50 dark:shadow-none dark:border-gray-700 dark:bg-gray-800 bg-opacity-50 shadow-2xl shadow-gray-600/10">
                         <h5 className="text-xl text-gray-700 dark:text-gray-300">
@@ -52,15 +46,8 @@ const CustomerStatistics = () => {
                         </div>    
                     </div> 
                 </div>               
-            
-
-            </div>
-
-            
-
+             </div>
         </div>
-
-              
     </> 
   );
 };
