cc4bf69b8475018333b51fe827e3596851f7c9c6
Yonaiker Ocando <yonaiker2410@gmail.com>
2023-10-03 15:39:52 -0400

    agregar endpoint ver clients y mostart lista de clientes

--- src/component/Clients/list.jsx
+++ src/component/Clients/list.jsx
@@ -1,27 +1,28 @@
 "use client";
-import React, { useState } from "react";
-import clients from "../../data/clients.json";
+import React, { useEffect, useState } from "react";
 import { BiSolidFilePdf } from "react-icons/bi";
 import { TbListDetails } from "react-icons/tb";
 
 /* use client */
 const list = () => {
-  const [currencyClient, setcurrencyClient] = useState(clients);
+  const [currentClients, setCurrentClients] = useState([]);
+  const [currentClient, setCurrentClient] = useState(null);
+
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
 
   const clientsPerPage = 15;
-  const totalPages = Math.ceil(clients.length / clientsPerPage);
+
+  const totalPages = Math.ceil(
+    ((currentClients && currentClients.length) || 0) / clientsPerPage
+  );
 
   // Calcula el índice inicial y final de los clientes a mostrar en la página actual
   const indexOfLastClient = currentPage * clientsPerPage;
   const indexOfFirstClient = indexOfLastClient - clientsPerPage;
-  const currentNowclients = clients.slice(
-    indexOfFirstClient,
-    indexOfLastClient
-  );
-
-  console.log(currencyClient);
+  const currentNowProducts = currentClients
+    ? currentClients.slice(indexOfFirstClient, indexOfLastClient)
+    : [];
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -30,6 +31,33 @@ const list = () => {
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
+
+  //obtener clientes reales de DB
+  const getClients = async () => {
+    try {
+      const res = await fetch("api/clients");
+      const product = await res.json();
+      console.log("res", res);
+      console.log("clients", product);
+
+      if (res.status !== 200) {
+        throw new Error(
+          `Error al obtener los clientes: ${product.error.message}`
+        );
+      }
+
+      setCurrentClients(product.data);
+    } catch (error) {
+      console.error("Error en la petición Fetch:", error);
+
+      toast.error(`Ocurrió un error: ${error.message}`);
+      setCurrentClients([]);
+    }
+  };
+
+  useEffect(() => {
+    getClients();
+  }, []);
 
   return (
     <>
@@ -57,13 +85,16 @@ const list = () => {
                 <th>ID</th>
                 <th>Nombre</th>
                 <th>Apellido</th>
-                <th>Cedula</th>
+                <th>Cédula</th>
+                <th>Correo</th>
+                <th>Celular</th>
+                <th>Dirección</th>
                 <th>Acción</th>
               </tr>
             </thead>
             <tbody>
               {/* row 1 */}
-              {currencyClient.map((clients) => (
+              {currentNowProducts.map((clients) => (
                 <tr>
                   <td>{clients.id}</td>
                   <td>
@@ -75,24 +106,28 @@ const list = () => {
                   </td>
                   <td>{clients.last_name}</td>
                   <td>{clients.ci}</td>
+                  <td>{clients.email}</td>
+                  <td>{clients.phone}</td>
+                  <td>{clients.address}</td>
+
                   <th>
-                  <div className="d-flex flex-row gap-4">
-                    <label
-                      htmlFor="my-modal"
-                      className="btn p-2 text-3xl"
-                      onClick={() => setCurrentProduct(product)}
-                    >
-                      <BiSolidFilePdf />
-                    </label>
-                    <label
-                      htmlFor="my-modal"
-                      className="btn p-2 text-3xl"
-                      onClick={() => setCurrentProduct(product)}
-                    >
-                      <TbListDetails />
-                    </label>
-                  </div>
-                </th>
+                    <div className="d-flex flex-row gap-4">
+                      <label
+                        htmlFor="my-modal"
+                        className="btn p-2 text-3xl"
+                        onClick={() => setCurrentClient(clients)}
+                      >
+                        <BiSolidFilePdf />
+                      </label>
+                      <label
+                        htmlFor="my-modal"
+                        className="btn p-2 text-3xl"
+                        onClick={() => setCurrentClient(clients)}
+                      >
+                        <TbListDetails />
+                      </label>
+                    </div>
+                  </th>
                 </tr>
               ))}
             </tbody>
@@ -102,7 +137,10 @@ const list = () => {
                 <th>ID</th>
                 <th>Nombre</th>
                 <th>Apellido</th>
-                <th>Cedula</th>
+                <th>Cédula</th>
+                <th>Correo</th>
+                <th>Celular</th>
+                <th>Dirección</th>
                 <th>Acción</th>
               </tr>
             </tfoot>
