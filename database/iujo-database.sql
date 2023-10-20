e8aaf801bcdf6591f1f1b935888560670d873287
aryclemente <hello.aryc@gmail.com>
2023-10-20 11:47:46 -0400

    Save

--- database/iujo-database.sql
+++ database/iujo-database.sql
@@ -82,5 +82,31 @@ CREATE TABLE "Detalles" (
     FOREIGN KEY ("id_producto")      REFERENCES "Productos"("id_producto")
 );
 
+INSERT INTO "Compañia" ("id_compañia", "Nombre", "Rif", "Dirección")
+VALUES 
+(uuid_generate_v4(), 'Apple', 45678589, 'USA Suite 54'),
+(uuid_generate_v4(), 'Alphabet', 234567, 'Canada PO Box 42303'),
+(uuid_generate_v4(), 'Meta', 85345678, 'Francia Room 1954'),
+(uuid_generate_v4(), 'Alibaba', 45566789, 'Australia Apt 110'),
+(uuid_generate_v4(), 'Amazon', 56782690, 'Singapur Room 925');
+
+INSERT INTO "Usuarios" ("Nombre", "Email", "Password", "id_compañia")
+VALUES 
+('Arianna', 'arianna@email.com', 'hashed_password', uuid_generate_v4()),
+('Yonaiker', 'yonaiker@email.com', 'hashed_password', uuid_generate_v4()),
+('Reynaldo', 'reynaldo@email.com', 'hashed_password', uuid_generate_v4()),
+
+;
+
+INSERT INTO "Clientes" ("Nombre", "Apellido", "CI", "Email", "Telefono", "phone_code")
+VALUES 
+('Juan', 'Pérez', 12345678, 'juan.perez@email.com', '+123456789', '+58'),
+('Ana', 'González', 23456789, 'ana.gonzalez@email.com', '+234567890', '+58'),
+('Carlos', 'Rodríguez', 34567890, 'carlos.rodriguez@email.com', '+345678901', '+58'),
+('Luisa', 'Martínez', 45678901, 'luisa.martinez@email.com', '+456789012', '+58'),
+('Maria', 'Fernández', 56789012, 'maria.fernandez@email.com', '+567890123', '+58');
+
+
+
 
 
